54585ba8a1d874eeccbd72bc63d980784f33e97b
004462 <a.velibeyli6@gmail.com>
2020-11-17 17:55:03 +0400

    working on it

---
+++ bestcommers-login-ms/src/main/java/app/rabbit/producer/UserExistenceQuestion.java
@@ -0,0 +1,17 @@
+package app.rabbit.producer;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+public class UserExistenceQuestion {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public UserExistenceQuestion(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void
+
+
+}
+
